f20284fa267e0f764f31d0aaddb0f6165f913745
Darshan Datta <darshandatta.10@gmail.com>
2019-12-31 00:13:21 +0530

    implemented firebase utils, included ability to store authenticated users in our firestore database

--- src/components/form-input/form-input.component.jsx
+++ src/components/form-input/form-input.component.jsx
@@ -5,14 +5,14 @@ import "./form-input.styles.scss";
 const FormInput = ({ handleChange, label, ...otherProps }) => (
   <div className="group">
     <input
-      id={otherProps.name}
+      id={otherProps.customfor || otherProps.name}
       className="form-input"
       onChange={handleChange}
       {...otherProps}
     />
     {label ? (
       <label
-        htmlFor={otherProps.name}
+        htmlFor={otherProps.customfor || otherProps.name}
         className={`${
           otherProps.value.length ? "shrink" : ""
         } form-input-label`}
